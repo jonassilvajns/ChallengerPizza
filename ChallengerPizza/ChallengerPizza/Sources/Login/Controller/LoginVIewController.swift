2afebfe70054b48cd146329fb5a0c58390b1251f
Jonas <jonassilvajns@icloud.com>
2022-09-21 23:50:20 -0300

    Feature: Creating APP iOS and files initial

---
+++ ChallengerPizza/ChallengerPizza/Sources/Login/Controller/LoginVIewController.swift
@@ -0,0 +1,33 @@
+//
+//  LoginVIewController.swift
+//  ChallengerPizza
+//
+//  Created by Jonas Silva on 21/09/22.
+//
+
+
+
+import UIKit
+
+
+class NameViewController: UIViewController {
+    
+    // MARK: - Propertties
+    
+    
+    
+    // MARK: - Constructors
+    
+    
+    
+    // MARK: - Methods
+    override func viewDidLoad() {
+    }
+    
+    
+}
+
+
+
+
+
---
+++ ChallengerPizza/ChallengerPizza/Sources/Login/View/LoginView.swift
@@ -0,0 +1,36 @@
+//
+//  LoginView.swift
+//  ChallengerPizza
+//
+//  Created by Jonas Silva on 21/09/22.
+//
+import UIKit
+
+
+class LoginView: UIView {
+    
+    // MARK: - Propertties
+    
+    private lazy var bannerImageView: UIImageView = {
+        var imageView = UIImageView()
+        
+        
+        return imageView
+    }()
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    // MARK: - Constructors
+    
+    
+    
+    // MARK: - Methods
+    
+    
+}
